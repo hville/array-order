43d286b1eadb60a5d7b362ae30757f4dd6e6a837
hville <hugov@runbox.com>
2021-02-20 22:19:25 -0500

    typedef and test

--- flip.js
+++ flip.js
@@ -1,10 +1,12 @@
 /**
+ * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
+ */
+/**
  * flip values and indices : rank => order || order => rank
  *
  * @example
  * 	const abc = flip( [2,0,1] ) // ==> [1,2,0]
  *
- * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
  * @param {ArrayLike} src array to be fliped
  * @param {ArrayLike} [tgt] destination else inplace
  * @return {ArrayLike}
--- tst/flip.js
+++ tst/flip.js
@@ -1,7 +1,7 @@
 import t from 'assert-op'
 import flip from '../flip.js'
-import sequence from '../sequence.js'
-import shuffle from '../shuffle.js'
+import sequence from '../util/sequence.js'
+import shuffle from '../util/shuffle.js'
 
 t('flip-to', a=>{
 	for (let i=1; i<10; ++i) {
