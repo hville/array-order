c7ffde21e156dfa0d011410a29aafc1095011a98
hville <hugov@runbox.com>
2021-02-20 22:19:03 -0500

    fixed naming convention

--- order.js
+++ order.js
@@ -1,5 +1,7 @@
-import rank from './rank.js'
-import flip from './rank.js'
+/**
+ * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
+ */
+import sequence from './util/sequence.js'
 
 /**
  * get an array representing the ordering i <= [i]
@@ -7,12 +9,15 @@ import flip from './rank.js'
  * @example
  * 	const from = order([1, 3, 0, 2]) // => [2, 0, 3, 1]
  *
- * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
  * @param {ArrayLike<number>} ref
  * @param {number|(a,b)=>number} [dir] +ascending | -descending | sort function
  * @return {ArrayLike}
  */
 export default function(ref, dir) {
-	return flip(rank(ref, dir))
+	return sequence(ref.length).sort(
+		typeof dir === 'function' ? (a,b) => dir( ref[a], ref[b] )
+		: dir < 0 ? (a,b) => ref[b] - ref[a]
+		: (a,b) => ref[a] - ref[b]
+	)
 }
 
--- rank.js
+++ rank.js
@@ -1,21 +1,20 @@
-import sequence from './util/sequence.js'
+/**
+ * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
+ */
+import order from './order.js'
+import flip from './flip.js'
 
 /**
- * get an array representing the ordering i <= [i]
+ * rank of source if sorted
  *
  * @example
- * 	const from = order([1, 3, 0, 2]) // => [2, 0, 3, 1]
+ * 	const ranked = rank([11, 13, 10, 12]) // => [1, 3, 0, 2]
  *
- * @typedef {Array|Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray|Float32Array|Float64Array} ArrayLike
- * @param {ArrayLike<number>} ref
+ * @param {ArrayLike} ref
  * @param {number|(a,b)=>number} [dir] +ascending | -descending | sort function
  * @return {ArrayLike}
  */
 export default function(ref, dir) {
-	return sequence(ref.length).sort(
-		typeof dir === 'function' ? (a,b) => dir( ref[a], ref[b] )
-		: dir < 0 ? (a,b) => ref[b] - ref[a]
-		: (a,b) => ref[a] - ref[b]
-	)
+	return flip(order(ref, dir))
 }
 
